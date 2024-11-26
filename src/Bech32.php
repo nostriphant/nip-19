a4561a10e2fbf972164da42797fe0fb57efe29ed
Rik Meijer <hello@rikmeijer.nl>
2024-11-26 12:56:22 +0100

    got custom bech32 encoding/decoding working

--- src/Bech32.php
+++ src/Bech32.php
@@ -2,45 +2,156 @@
 
 namespace nostriphant\NIP19;
 
-use function BitWasp\Bech32\convertBits;
-use function BitWasp\Bech32\decode;
-use function BitWasp\Bech32\encode;
+readonly class Bech32 {
 
-class Bech32 {
+    public array $data;
+
+    public function __construct(public string $type, mixed ...$data) {
+        $this->data = $data;
+    }
+
+    public function __toString(): string {
+        return self::encodeRaw($this->type, match ($this->type) {
+                    'nsec' => self::fromHexToBytes($this->data[0]),
+                    'npub' => self::fromHexToBytes($this->data[0]),
+                    'note' => self::fromHexToBytes($this->data[0]),
+                    'nprofile' => self::encodeTLV(
+                            [self::fromHexToBytes($this->data['pubkey'])],
+                            array_map([self::class, 'fromUTF8ToBytes'], $this->data['relays'] ?? [])
+                    ),
+                    'naddr' => self::encodeTLV(
+                            [self::fromUTF8ToBytes($this->data['identifier'])],
+                            array_map([self::class, 'fromUTF8ToBytes'], $this->data['relays'] ?? []),
+                            [self::fromHexToBytes($this->data['pubkey'])],
+                            [self::fromIntegerToBytes($this->data['kind'])],
+                    ),
+                    'nevent' => self::encodeTLV(
+                            [self::fromHexToBytes($this->data['id'])],
+                            array_map([self::class, 'fromUTF8ToBytes'], $this->data['relays'] ?? []),
+                            isset($this->data['author']) ? [self::fromHexToBytes($this->data['author'])] : [],
+                            [self::fromIntegerToBytes($this->data['kind'])],
+                    )
+        });
+    }
+
+    static function decode(string $bech32): self {
+        list($hrp, $decoded) = self::decodeRaw($bech32, 5000);
 
 
-    private static function convertBech32ToHex(#[\SensitiveParameter] string $bech32_key): string {
-        $str = '';
-        try {
-            $decoded = decode($bech32_key);
-            $data = $decoded[1];
-            $bytes = convertBits($data, count($data), 5, 8, false);
-            foreach ($bytes as $item) {
-                $str .= str_pad(dechex($item), 2, '0', STR_PAD_LEFT);
-            }
-        } catch (Bech32Exception) {
 
+        return new self(...array_merge(match ($hrp) {
+                    'nsec' => [$hrp, self::fromBytesToHex(self::decodeBits($decoded))],
+                    'npub' => [$hrp, self::fromBytesToHex(self::decodeBits($decoded))],
+                    'note' => [$hrp, self::fromBytesToHex(self::decodeBits($decoded))],
+                    'nprofile' => self::parseTLVNProfile($decoded),
+                    'naddr' => self::parseTLVNAddr($decoded)
+                }));
+    }
+
+    static function array_entries(array $array) {
+        return array_map(fn(mixed $key, mixed $value) => [$key, $value], array_keys($array), array_values($array));
+    }
+
+    static function parseTLVNProfile(array $data): array {
+        $tlv = self::parseTLV($data);
+        if (!isset($tlv[0]) || !isset($tlv[0][0])) {
+            throw new \Exception('missing TLV 0 for nprofile');
+        }
+        if (count($tlv[0][0]) !== 32) {
+            throw new \Exception('TLV 0 should be 32 bytes');
         }
 
-        return $str;
+        return [
+            "type" => 'nprofile',
+            "pubkey" => self::fromBytesToHex($tlv[0][0]),
+            "relays" => $tlv[1] ? array_map([self::class, 'fromBytesToUTF8'], $tlv[1]) : [],
+        ];
+    }
+
+    static function parseTLVNAddr(array $data): array {
+        $tlv = self::parseTLV($data);
+        return [
+            "type" => 'naddr',
+            "identifier" => self::fromBytesToUTF8($tlv[0][0]),
+            "pubkey" => self::fromBytesToHex($tlv[2][0]),
+            "kind" => self::fromBytesToInteger($tlv[3][0]),
+            "relays" => isset($tlv[1]) ? array_map([self::class, 'fromBytesToUTF8'], $tlv[1]) : []
+        ];
+    }
+
+    static function parseTLV(array $bits): array {
+        $result = [];
+        $rest = self::decodeBits($bits);
+        while (count($rest) > 0) {
+            $type = array_shift($rest);
+            $length = array_shift($rest);
+            $value = array_slice($rest, 0, $length);
+            if (count($value) < $length) {
+                throw new \Exception('not enough data to read on TLV ' . $type);
+            }
+            $rest = array_slice($rest, $length);
+            $result[$type] = $result[$type] ?? [];
+            $result[$type][] = $value;
+        }
+        return $result;
+    }
+
+    static function encodeTLV(array ...$tlv): array {
+        return array_reduce(self::array_entries($tlv), function (array $carry, array $tlv_entry): array {
+            return array_reduce($tlv_entry[1], function (array $carry, array $value) use ($tlv_entry): array {
+                return array_merge($carry, [$tlv_entry[0], count($value)], $value);
+            }, $carry);
+        }, []);
+    }
+
+    static function fromBytesToHex(array $bytes): string {
+        return array_reduce($bytes, fn(string $hex, int $item) => $hex .= str_pad(dechex($item), 2, '0', STR_PAD_LEFT), '');
+    }
+
+    static function fromBytesToInteger(array $bytes): int {
+        return hexdec(self::fromBytesToHex($bytes));
+    }
+
+    static function fromBytesToUTF8(array $bytes): string {
+        return array_reduce($bytes, fn(string $utf8, int $item) => $utf8 .= chr($item), '');
+    }
+
+    private static function convertBech32ToHex(#[\SensitiveParameter] string $bech32_key): string {
+        try {
+            return self::decode($bech32_key)->data[0];
+        } catch (\Exception) {
+            return '';
+        }
+    }
+
+
+    static function fromHexToBytes(#[\SensitiveParameter] string $hex_key): array {
+        return array_map('hexdec', str_split($hex_key, 2));
+    }
+
+    static function fromUTF8ToBytes(string $utf8): array {
+        return array_map('ord', mb_str_split($utf8));
+    }
+
+    static function fromIntegerToBytes(int $integer): array {
+        // Create a Uint8Array with enough space to hold a 32-bit integer (4 bytes).
+        $uint8Array = [];
+
+        // Use bitwise operations to extract the bytes.
+        $uint8Array[0] = ($integer >> 24) & 0xff; // Most significant byte (MSB)
+        $uint8Array[1] = ($integer >> 16) & 0xff;
+        $uint8Array[2] = ($integer >> 8) & 0xff;
+        $uint8Array[3] = $integer & 0xff; // Least significant byte (LSB)
+
+        return $uint8Array;
     }
 
     private static function convertHexToBech32(#[\SensitiveParameter] string $hex_key, string $prefix) {
-        $str = '';
-
         try {
-            $dec = [];
-            $split = str_split($hex_key, 2);
-            foreach ($split as $item) {
-                $dec[] = hexdec($item);
-            }
-            $bytes = convertBits($dec, count($dec), 8, 5);
-            $str = encode($prefix, $bytes);
-        } catch (Bech32Exception) {
-
+            return '' . (new self($prefix, $hex_key));
+        } catch (\Exception) {
+            return '';
         }
-
-        return $str;
     }
 
     static function toNpub(string $hex) {
@@ -58,5 +169,179 @@ class Bech32 {
 
     static function fromNsec(#[\SensitiveParameter] string $nsec): string {
         return self::convertBech32ToHex($nsec, 'nsec');
+    }
+
+    static function isValid(string $expected_type, string $bech32) {
+        try {
+            $decoded = decode($bech32);
+        } catch (\Exception $ex) {
+            return false;
+        }
+        return $decoded[0] === $expected_type;
+    }
+
+    static function isValidNProfile(string $bech32): bool {
+        return self::isValid('nprofile', $bech32);
+    }
+
+    static function isValidNAddress(string $bech32): bool {
+        return self::isValid('naddress', $bech32);
+    }
+
+    static function isValidNSec(string $bech32): bool {
+        return self::isValid('nsec', $bech32);
+    }
+
+    static function isValidNPub(string $bech32): bool {
+        return self::isValid('npub', $bech32);
+    }
+
+    static function isValidNote(string $bech32): bool {
+        return self::isValid('note', $bech32);
+    }
+
+    static function isValidNCryptSec(string $bech32): bool {
+        return self::isValid('ncryptsec', $bech32);
+    }
+
+    static function isValidNEvent(string $bech32): bool {
+        return self::isValid('nevent', $bech32);
+    }
+
+    const GENERATOR = [0x3b6a57b2, 0x26508e6d, 0x1ea119fa, 0x3d4233dd, 0x2a1462b3];
+
+    static function polyMod(array $values, int $numValues): int {
+        $chk = 1;
+        for ($i = 0; $i < $numValues; $i++) {
+            $top = $chk >> 25;
+            $chk = ($chk & 0x1ffffff) << 5 ^ $values[$i];
+
+            for ($j = 0; $j < 5; $j++) {
+                $value = (($top >> $j) & 1) ? self::GENERATOR[$j] : 0;
+                $chk ^= $value;
+            }
+        }
+
+        return $chk;
+    }
+
+    static function hrpExpand(string $hrp, int $hrpLen): array {
+        $expand1 = [];
+        $expand2 = [];
+        for ($i = 0; $i < $hrpLen; $i++) {
+            $o = ord($hrp[$i]);
+            $expand1[] = $o >> 5;
+            $expand2[] = $o & 31;
+        }
+
+        return array_merge($expand1, [0], $expand2);
+    }
+
+    static function decodeBits(array $data) {
+        return self::convertBits($data, 5, 8, false);
+    }
+
+    static function encodeBits(array $data) {
+        return self::convertBits($data, 8, 5, true);
+    }
+
+    static function convertBits(array $data, int $fromBits, int $toBits, bool $pad = true): array {
+        $inLen = count($data);
+        $acc = 0;
+        $bits = 0;
+        $ret = [];
+        $maxv = (1 << $toBits) - 1;
+        $maxacc = (1 << ($fromBits + $toBits - 1)) - 1;
+
+        for ($i = 0; $i < $inLen; $i++) {
+            $value = $data[$i];
+            if ($value < 0 || $value >> $fromBits) {
+                throw new \Exception('Invalid value for convert bits');
+            }
+
+            $acc = (($acc << $fromBits) | $value) & $maxacc;
+            $bits += $fromBits;
+
+            while ($bits >= $toBits) {
+                $bits -= $toBits;
+                $ret[] = (($acc >> $bits) & $maxv);
+            }
+        }
+
+        if ($pad) {
+            if ($bits) {
+                $ret[] = ($acc << $toBits - $bits) & $maxv;
+            }
+        } else if ($bits >= $fromBits || ((($acc << ($toBits - $bits))) & $maxv)) {
+            throw new \Exception('Invalid data');
+        }
+
+        return $ret;
+    }
+
+    static function createChecksum(string $hrp, array $convertedDataChars): array {
+        $values = array_merge(self::hrpExpand($hrp, strlen($hrp)), $convertedDataChars);
+        $polyMod = self::polyMod(array_merge($values, [0, 0, 0, 0, 0, 0]), count($values) + 6) ^ 1;
+        $results = [];
+        for ($i = 0; $i < 6; $i++) {
+            $results[$i] = ($polyMod >> 5 * (5 - $i)) & 31;
+        }
+
+        return $results;
+    }
+
+    static function verifyChecksum(string $hrp, array $convertedDataChars): bool {
+        $expandHrp = self::hrpExpand($hrp, strlen($hrp));
+        $r = array_merge($expandHrp, $convertedDataChars);
+        return self::polyMod($r, count($r)) === 1;
+    }
+
+    const CHARSET = 'qpzry9x8gf2tvdw0s3jn54khce6mua7l';
+
+    static function encodeRaw(string $hrp, array $bytes): string {
+        $words = self::encodeBits($bytes);
+        $checksum = self::createChecksum($hrp, $words);
+        $characters = array_merge($words, $checksum);
+
+        $encoded = [];
+        for ($i = 0, $n = count($characters); $i < $n; $i++) {
+            if (isset(self::CHARSET[$characters[$i]]) === false) {
+                var_dump($hrp, $words);
+            }
+            $encoded[$i] = self::CHARSET[$characters[$i]];
+        }
+
+        return "{$hrp}1" . implode('', $encoded);
+    }
+
+    static function decodeRaw(string $sBech, int $limit = 90): array {
+        $length = strlen($sBech);
+
+        if ($length < 8 || $length > $limit) {
+            throw new \InvalidArgumentException("invalid string length: $length ($sBech). Expected (8..$limit)");
+        }
+        // don't allow mixed case
+        $lowered = strtolower($sBech);
+        if ($sBech !== $lowered && $sBech !== strtoupper($sBech)) {
+            throw new \InvalidArgumentException("String must be lowercase or uppercase");
+        }
+
+        $sepIndex = strrpos($sBech, '1');
+        if ($sepIndex === false) {
+            throw new \InvalidArgumentException("Letter '1' must be present between prefix and data ($sBech) only");
+        }
+
+        list($prefix, $data) = explode('1', $sBech, 2);
+        if (strlen($data) < 6) {
+            throw new \InvalidArgumentException('Bech32 data must be at least 6 characters long');
+        }
+
+        $words = array_map(fn(string $char) => strpos(self::CHARSET, $char), str_split(substr($data, 0, -6)));
+        $sum = self::verifyChecksum($prefix, $words);
+        if (str_ends_with($data, $sum) === false) {
+            throw new \Exception("Invalid checksum in $sBech: expected '$sum'.");
+        }
+
+        return [$prefix, $words];
     }
 }
--- tests/NIP19Test.php
+++ tests/NIP19Test.php
@@ -3,6 +3,7 @@
 use nostriphant\NIP19\Bech32;
 
 it('converts between bech32 and hexidecimal', function () {
+
     $public_key_hex = '7e7e9c42a91bfef19fa929e5fda1b72e0ebc1a4c1141673e2794234d86addf4e';
     $public_key_bech32 = 'npub10elfcs4fr0l0r8af98jlmgdh9c8tcxjvz9qkw038js35mp4dma8qzvjptg';
     $private_key_hex = '67dea2ed018072d675f5415ecfaed7d2597555e202d85b3d65ea4e58d2d92ffa';
@@ -14,3 +15,177 @@ it('converts between bech32 and hexidecimal', function () {
     expect(Bech32::fromNsec($private_key_bech32))->toBe($private_key_hex);
     expect(Bech32::toNsec($private_key_hex))->toBe($private_key_bech32);
 });
+//;
+//import { test, expect, describe } from 'bun:test';
+//import { generateSecretKey, getPublicKey } from './pure.ts';
+//import {
+//  decode,
+//  naddrEncode,
+//  Bech32::decode,
+//  npubEncode,
+//  nsecEncode,
+//  neventEncode,
+//  type AddressPointer,
+//  type ProfilePointer,
+//  EventPointer,
+//  NostrTypeGuard,
+//} from './nip19.ts';
+
+it('encodes and decodes nsec', function () {
+    $private_key_hex = '67dea2ed018072d675f5415ecfaed7d2597555e202d85b3d65ea4e58d2d92ffa';
+    $nsec = Bech32::toNsec($private_key_hex);
+    $bech32 = Bech32::decode($nsec);
+    expect('' . $bech32)->toMatch('/nsec1\w+/');
+    expect($bech32->type)->toEqual('nsec');
+    expect($bech32->data[0])->toEqual($private_key_hex);
+});
+
+it('encodes and decodes npub', function () {
+    $public_key_hex = '7e7e9c42a91bfef19fa929e5fda1b72e0ebc1a4c1141673e2794234d86addf4e';
+    $npub = Bech32::toNpub($public_key_hex);
+    $bech32 = Bech32::decode($npub);
+    expect('' . $bech32)->toMatch('/npub1\w+/');
+        expect($bech32->type)->toEqual('npub');
+    expect($bech32->data[0])->toEqual($public_key_hex);
+});
+
+it('encodes and decodes nprofile', function () {
+
+    $public_key_hex = '7e7e9c42a91bfef19fa929e5fda1b72e0ebc1a4c1141673e2794234d86addf4e';
+    $relays = ['wss://relay.nostr.example.mydomain.example.com', 'wss://nostr.banana.com'];
+    $bech32 = new Bech32('nprofile', pubkey: $public_key_hex, relays: $relays);
+    expect('' . $bech32)->toMatch('/nprofile1\w+/');
+        expect($bech32->type)->toEqual('nprofile');
+    expect($bech32->data['pubkey'])->toEqual($public_key_hex);
+    expect($bech32->data['relays'])->toContain('wss://relay.nostr.example.mydomain.example.com');
+    expect($bech32->data['relays'])->toContain('wss://nostr.banana.com');
+});
+
+it('decodes nprofile without relays', function () {
+    $public_key_hex = '97c70a44366a6535c145b333f973ea86dfdc2d7a99da618c40c64705ad98e322';
+    $relays = [];
+    $bech32 = new Bech32('nprofile', pubkey: $public_key_hex, relays: $relays);
+    expect('' . $bech32)->toMatch('/nprofile1\w+/');
+    expect($bech32->type)->toEqual('nprofile');
+    expect($bech32->data['pubkey'])->toEqual($public_key_hex);
+    expect($bech32->data['relays'])->toBeEmpty();
+});
+
+it('encode and decode naddr', function () {
+
+    $public_key_hex = '7e7e9c42a91bfef19fa929e5fda1b72e0ebc1a4c1141673e2794234d86addf4e';
+    $relays = ['wss://relay.nostr.example.mydomain.example.com', 'wss://nostr.banana.com'];
+    $bech32 = new Bech32('naddr',
+            pubkey: $public_key_hex,
+            relays: $relays,
+            kind: 30023,
+            identifier: 'banana'
+    );
+    expect('' . $bech32)->toMatch('/naddr1\w+/');
+        expect($bech32->type)->toEqual('naddr');
+    expect($bech32->data['pubkey'])->toEqual($public_key_hex);
+    expect($bech32->data['relays'])->toContain($relays[0]);
+    expect($bech32->data['relays'])->toContain($relays[1]);
+    expect($bech32->data['kind'])->toEqual(30023);
+    expect($bech32->data['identifier'])->toEqual('banana');
+});
+
+it('encode and decode nevent', function () {
+
+    $public_key_hex = '7e7e9c42a91bfef19fa929e5fda1b72e0ebc1a4c1141673e2794234d86addf4e';
+    $relays = ['wss://relay.nostr.example.mydomain.example.com', 'wss://nostr.banana.com'];
+    $bech32 = new Bech32('nevent',
+            id: $public_key_hex,
+            relays: $relays,
+            kind: 30023,
+    );
+    expect('' . $bech32)->toMatch('/nevent1\w+/');
+    expect($bech32->type)->toEqual('nevent');
+    expect($bech32->data['id'])->toEqual($public_key_hex);
+    expect($bech32->data['relays'])->toContain($relays[0]);
+    expect($bech32->data['kind'])->toEqual(30023);
+});
+
+it('encode and decode nevent with kind 0', function () {
+
+    $public_key_hex = '7e7e9c42a91bfef19fa929e5fda1b72e0ebc1a4c1141673e2794234d86addf4e';
+    $relays = ['wss://relay.nostr.example.mydomain.example.com', 'wss://nostr.banana.com'];
+    $bech32 = new Bech32('nevent',
+            id: $public_key_hex,
+            relays: $relays,
+            kind: 0,
+    );
+    expect('' . $bech32)->toMatch('/nevent1\w+/');
+    expect($bech32->type)->toEqual('nevent');
+    expect($bech32->data['id'])->toEqual($public_key_hex);
+    expect($bech32->data['relays'])->toContain($relays[0]);
+    expect($bech32->data['kind'])->toEqual(0);
+});
+
+it('encode and decode naddr with empty "d"', function () {
+
+    $public_key_hex = '7e7e9c42a91bfef19fa929e5fda1b72e0ebc1a4c1141673e2794234d86addf4e';
+    $relays = ['wss://relay.nostr.example.mydomain.example.com', 'wss://nostr.banana.com'];
+    $bech32 = new Bech32('naddr',
+            identifier: '',
+            pubkey: $public_key_hex,
+            relays: $relays,
+            kind: 3,
+    );
+    expect('' . $bech32)->toMatch('/naddr\w+/');
+    expect($bech32->type)->toEqual('naddr');
+    expect($bech32->data['identifier'])->toEqual('');
+    expect($bech32->data['relays'])->toContain('wss://relay.nostr.example.mydomain.example.com');
+    expect($bech32->data['kind'])->toEqual(3);
+    expect($bech32->data['pubkey'])->toEqual($public_key_hex);
+});
+
+it('decode naddr from habla.news', function () {
+
+    $bech32 = Bech32::decode(
+            'naddr1qq98yetxv4ex2mnrv4esygrl54h466tz4v0re4pyuavvxqptsejl0vxcmnhfl60z3rth2xkpjspsgqqqw4rsf34vl5',
+    );
+    expect($bech32->type)->toEqual('naddr');
+    expect($bech32->data['pubkey'])->toEqual('7fa56f5d6962ab1e3cd424e758c3002b8665f7b0d8dcee9fe9e288d7751ac194');
+    expect($bech32->data['kind'])->toEqual(30023);
+    expect($bech32->data['identifier'])->toEqual('references');
+});
+
+it('decode naddr from go-nostr with different TLV ordering', function () {
+
+    $bech32 = Bech32::decode(
+            'naddr1qqrxyctwv9hxzq3q80cvv07tjdrrgpa0j7j7tmnyl2yr6yr7l8j4s3evf6u64th6gkwsxpqqqp65wqfwwaehxw309aex2mrp0yhxummnw3ezuetcv9khqmr99ekhjer0d4skjm3wv4uxzmtsd3jjucm0d5q3vamnwvaz7tmwdaehgu3wvfskuctwvyhxxmmd0zfmwx',
+    );
+
+    expect($bech32->type)->toEqual('naddr');
+    expect($bech32->data['pubkey'])->toEqual('3bf0c63fcb93463407af97a5e5ee64fa883d107ef9e558472c4eb9aaaefa459d');
+    expect($bech32->data['relays'])->toContain('wss://relay.nostr.example.mydomain.example.com');
+    expect($bech32->data['relays'])->toContain('wss://nostr.banana.com');
+    expect($bech32->data['kind'])->toEqual(30023);
+    expect($bech32->data['identifier'])->toEqual('banana');
+});
+
+//it('is valid Bech32', function (string $method, string $bech32, bool $expected) {
+//    expect(Bech32::$method($bech32))->toBe($expected, $method . ': ' . $bech32);
+//})->with([
+//    ['isValidNProfile', 'nprofile1qqsvc6ulagpn7kwrcwdqgp797xl7usumqa6s3kgcelwq6m75x8fe8yc5usxdg', true],
+//    ['isValidNProfile', 'nprofile1qqsvc6ulagpn7kwrcwdqgp797xl7usumqa6s3kgcelwq6m75x8fe8yc5usxãg', false],
+//    ['isValidNProfile', 'nsec1lqw6zqyanj9mz8gwhdam6tqge42vptz4zg93qsfej440xm5h5esqya0juv', false],
+//    ['isValidNEvent', 'nevent1qqst8cujky046negxgwwm5ynqwn53t8aqjr6afd8g59nfqwxpdhylpcpzamhxue69uhhyetvv9ujuetcv9khqmr99e3k7mg8arnc9', true],
+//    ['isValidNEvent', 'nevent1qqst8cujky046negxgwwm5ynqwn53t8aqjr6afd8g59nfqwxpdhylpcpzamhxue69uhhyetvv9ujuetcv9khqmr99e3k7mg8ãrnc9', false],
+//    ['isValidNEvent', 'nprofile1qqsvc6ulagpn7kwrcwdqgp797xl7usumqa6s3kgcelwq6m75x8fe8yc5usxdg', false],
+//    ['isValidNAddress', 'naddr1qqxnzdesxqmnxvpexqunzvpcqyt8wumn8ghj7un9d3shjtnwdaehgu3wvfskueqzypve7elhmamff3sr5mgxxms4a0rppkmhmn7504h96pfcdkpplvl2jqcyqqq823cnmhuld', true],
+//    ['isValidNAddress', 'nsec1lqw6zqyanj9mz8gwhdam6tqge42vptz4zg93qsfej440xm5h5esqya0juv', false],
+//    ['isValidNSec', 'nsec1lqw6zqyanj9mz8gwhdam6tqge42vptz4zg93qsfej440xm5h5esqya0juv', true],
+//    ['isValidNSec', 'nsec1lqw6zqyanj9mz8gwhdam6tqge42vptz4zg93qsfej440xm5h5esqya0juã', false],
+//    ['isValidNSec', 'nprofile1qqsvc6ulagpn7kwrcwdqgp797xl7usumqa6s3kgcelwq6m75x8fe8yc5usxdg', false],
+//    ['isValidNPub', 'npub1jz5mdljkmffmqjshpyjgqgrhdkuxd9ztzasv8xeh5q92fv33sjgqy4pats', true],
+//    ['isValidNPub', 'npub1jz5mdljkmffmqjshpyjgqgrhdkuxd9ztzãsv8xeh5q92fv33sjgqy4pats', false],
+//    ['isValidNPub', 'nsec1lqw6zqyanj9mz8gwhdam6tqge42vptz4zg93qsfej440xm5h5esqya0juv', false],
+//    ['isValidNote', 'note1gmtnz6q2m55epmlpe3semjdcq987av3jvx4emmjsa8g3s9x7tg4sclreky', true],
+//    ['isValidNote', 'note1gmtnz6q2m55epmlpe3semjdcq987av3jvx4emmjsa8g3s9x7tg4sçlreky', false],
+//    ['isValidNote', 'npub1jz5mdljkmffmqjshpyjgqgrhdkuxd9ztzasv8xeh5q92fv33sjgqy4pats', false],
+//    ['isValidNCryptSec', 'ncryptsec1qgg9947rlpvqu76pj5ecreduf9jxhselq2nae2kghhvd5g7dgjtcxfqtd67p9m0w57lspw8gsq6yphnm8623nsl8xn9j4jdzz84zm3frztj3z7s35vpzmqf6ksu8r89qk5z2zxfmu5gv8th8wclt0h4p', true],
+//    ['isValidNCryptSec', 'ncryptsec1qgg9947rlpvqu76pj5ecreduf9jxhselq2nae2kghhvd5g7dgjtcxfqtd67p9m0w57lspw8gsq6yphnm8623nsã8xn9j4jdzz84zm3frztj3z7s35vpzmqf6ksu8r89qk5z2zxfmu5gv8th8wclt0h4p', false],
+//    ['isValidNCryptSec', 'note1gmtnz6q2m55epmlpe3semjdcq987av3jvx4emmjsa8g3s9x7tg4sçlreky', false]
+//]);
